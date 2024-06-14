0396757bf896252db8ee8bafb1086d9790d5f326
smoulderpipe <del.v.paola@gmail.com>
2024-06-14 10:45:49 +0200

    feat: implemented email control in register system

--- focufy-frontend/src/app/components/register/register.component.ts
+++ focufy-frontend/src/app/components/register/register.component.ts
@@ -9,52 +9,66 @@ import { passwordMatchValidator } from 'src/app/validators/validators';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit{
-    registerForm!: FormGroup;
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-    private baseUrl = 'http://localhost:8080/auth/register';
+  private baseUrl = 'http://localhost:8080/auth/register';
 
-    constructor(private http: HttpClient){
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    ngOnInit(): void {
-      this.registerForm = new FormGroup({
-        name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(30)]),
-        passwordConf: new FormControl(null, [
-          Validators.required,
-          Validators.minLength(8), Validators.maxLength(30),
-          passwordMatchValidator('password')
-        ])
-      });
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(30)]),
+      passwordConf: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8), Validators.maxLength(30),
+        passwordMatchValidator('password')
+      ])
+    });
+
+    this.registerForm.get('password')?.valueChanges.subscribe(passwordValue => {
+      this.registerForm.get('passwordConf')?.updateValueAndValidity();
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm && this.registerForm.valid) {
+      const user: User = {
+        name: this.registerForm.value.name,
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password
+      };
   
-      this.registerForm.get('password')?.valueChanges.subscribe(passwordValue => {
-        this.registerForm.get('passwordConf')?.updateValueAndValidity();
-      });
-    }
-
-    onSubmit() {
-      if (this.registerForm && this.registerForm.valid) {
-        const user: User = {
-          name: this.registerForm.value.name,
-          email: this.registerForm.value.email,
-          password: this.registerForm.value.password
-        };
-  
-        this.http.post(this.baseUrl, user)
-          .subscribe(response => {
+      this.http.post(this.baseUrl, user)
+        .subscribe(
+          response => {
             console.log('Utente creato con successo:', response);
             alert('Grazie per esserti registrato!');
             window.location.href = "/login";
-          }, error => {
-            console.error('Errore nella creazione dell utente:', error);
-            alert('Si è verificato un errore, riprovare più tardi');
-          });
-      } else {
-        console.error('Il form non è valido!');
-      }
+          },
+          error => {
+            if (error.status === 400) {
+              const errorMessage = error.error;
+              const regex = /Email.*is already in use\./i;
+              if (regex.test(errorMessage)) {
+                alert('Email già registrata. Prova con un altro indirizzo email.');
+              } else {
+                console.error('Errore nella creazione dell utente:', error);
+                alert('Si è verificato un errore, riprovare più tardi');
+              }
+            } else {
+              console.error('Errore nella creazione dell utente:', error);
+              alert('Si è verificato un errore, riprovare più tardi');
+            }
+          }
+        );
+    } else {
+      console.error('Il form non è valido!');
     }
+  }
 
-    
+
 }
