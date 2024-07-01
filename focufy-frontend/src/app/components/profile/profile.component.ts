93789052ad36d5dee111f0eab051907b5f266e92
smoulderpipe <del.v.paola@gmail.com>
2024-07-01 18:28:02 +0200

    feat: implemented same logic of previous commit on profile component

--- focufy-frontend/src/app/components/profile/profile.component.ts
+++ focufy-frontend/src/app/components/profile/profile.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Avatar } from 'src/app/models/avatar';
+import { Question } from 'src/app/models/question';
 import { StudyPlan } from 'src/app/models/studyPlan';
 import { User } from 'src/app/models/user';
+import { AnswerService } from 'src/app/services/answer.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { StudyPlanService } from 'src/app/services/study-plan.service';
 import { UserService } from 'src/app/services/user.service';
@@ -15,11 +18,13 @@ export class ProfileComponent implements OnInit {
   user: User | null = null;
   avatar: Avatar | null = null;
   studyPlan: StudyPlan | null = null;
-
+  
   constructor(
     private authService: AuthService,
     private userService: UserService,
-    private studyPlanService: StudyPlanService
+    private studyPlanService: StudyPlanService,
+    private answerService: AnswerService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -52,5 +57,38 @@ export class ProfileComponent implements OnInit {
         }
       );
     }
+  }
+
+  confirmRestart() {
+    const confirmation = confirm(`Would you like to start a new adventure? \n\nWARNING \nBy proceeding, you will PERMANENTLY DELETE your study plan, your avatar and your goals.`);
+    if (confirmation) {
+      this.onRestartAnswer();
+    } else {
+      // Azioni da eseguire se l'utente annulla l'operazione
+    }
+  }
+
+  onRestartAnswer() {
+    const userId = this.authService.getUserId();
+    if (!userId) {
+      console.error('User ID not found');
+      return;
+    }
+  
+    const answers: any[] = [];
+  
+    // Esegui la logica per costruire le risposte basate sulla domanda
+    // Se non hai bisogno di question qui, non c'è bisogno di utilizzarla
+  
+    this.answerService.savePersonalAnswers(answers).subscribe(
+      (response) => {
+        console.log('Restart answers submitted successfully', response);
+        this.router.navigate(['/survey']);
+      },
+      (error) => {
+        console.error('Error submitting restart answers', error);
+        this.router.navigate(['/survey']);
+      }
+    );
   }
 }
