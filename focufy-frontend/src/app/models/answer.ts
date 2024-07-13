b711f251284e64127536acda903d93a8d89bfb41
smoulderpipe <del.v.paola@gmail.com>
2024-07-13 16:47:13 +0200

    refactor - reformatted some files

--- focufy-frontend/src/app/models/answer.ts
+++ focufy-frontend/src/app/models/answer.ts
@@ -1,37 +1,37 @@
 import { Question } from "./question";
 
 export enum PersonalAnswerType {
-    DAYS = 'DAYS',
-    SHORT_TERM_GOAL = 'SHORT_TERM_GOAL',
-    LONG_TERM_GOAL = 'LONG_TERM_GOAL',
-    SATISFACTION = 'SATISFACTION',
-    CHECKPOINT = 'CHECKPOINT',
-    DEADLINE = 'DEADLINE',
-    RESTART = 'RESTART'
-  }
+  DAYS = 'DAYS',
+  SHORT_TERM_GOAL = 'SHORT_TERM_GOAL',
+  LONG_TERM_GOAL = 'LONG_TERM_GOAL',
+  SATISFACTION = 'SATISFACTION',
+  CHECKPOINT = 'CHECKPOINT',
+  DEADLINE = 'DEADLINE',
+  RESTART = 'RESTART'
+}
 
-  export enum SharedAnswerType {
-    LION = 'LION',
-    BEAR = 'BEAR',
-    DOLPHIN = 'DOLPHIN',
-    WOLF = 'WOLF',
-    WHIMSICAL = 'WHIMSICAL',
-    TENACIOUS = 'TENACIOUS',
-    TACTICAL = 'TACTICAL',
-    GREEDY = 'GREEDY'
-  }
+export enum SharedAnswerType {
+  LION = 'LION',
+  BEAR = 'BEAR',
+  DOLPHIN = 'DOLPHIN',
+  WOLF = 'WOLF',
+  WHIMSICAL = 'WHIMSICAL',
+  TENACIOUS = 'TENACIOUS',
+  TACTICAL = 'TACTICAL',
+  GREEDY = 'GREEDY'
+}
 
-  export enum CDAnswerType {
-    STUDY = 'STUDY',
-    FUN = 'FUN',
-    REST = 'REST'
-  }
+export enum CDAnswerType {
+  STUDY = 'STUDY',
+  FUN = 'FUN',
+  REST = 'REST'
+}
 
-  export interface CheckpointAnswer {
-    questionId: number;
-    answerText: string;
-    checkpointDayId: number;
-    answerType: CDAnswerType;
+export interface CheckpointAnswer {
+  questionId: number;
+  answerText: string;
+  checkpointDayId: number;
+  answerType: CDAnswerType;
 }
 
 export interface DeadlineAnswer {
@@ -41,11 +41,11 @@ export interface DeadlineAnswer {
   answerType: CDAnswerType;
 }
 
-export interface Answer{
-    id: number;
-    answerText: string;
-    question: Question;
-    personalAnswerType: PersonalAnswerType;
-    sharedAnswerType: SharedAnswerType;
-    
+export interface Answer {
+  id: number;
+  answerText: string;
+  question: Question;
+  personalAnswerType: PersonalAnswerType;
+  sharedAnswerType: SharedAnswerType;
+
 }
