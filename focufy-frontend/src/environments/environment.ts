5f73aaf923ba1613c63f8497be8323686c75f992
smoulderpipe <del.v.paola@gmail.com>
2024-08-06 13:32:54 +0200

    test: edited environment variable for web service backend koyeb

--- focufy-frontend/src/environments/environment.ts
+++ focufy-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://economic-emily-smoulderpipe-92a2c37b.koyeb.app/'
+    baseUrl: 'https://economic-emily-smoulderpipe-92a2c37b.koyeb.app'
   };
