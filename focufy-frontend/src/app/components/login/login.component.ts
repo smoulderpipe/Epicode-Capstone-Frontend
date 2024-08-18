151ce5f2f73a074385902ecd90af0be35c3aaa75
smoulderpipe <del.v.paola@gmail.com>
2024-08-18 22:42:50 +0200

    test: same as previous commit

--- focufy-frontend/src/app/components/login/login.component.ts
+++ focufy-frontend/src/app/components/login/login.component.ts
@@ -66,17 +66,20 @@ export class LoginComponent implements OnInit {
             });
           } else {
             console.error('User ID not found');
-            this.modalTitle = "Oops!"
-            this.modalDescription = "We couldn't find an account with that email. Want to try again?"
+            this.modalTitle = "Oops!";
+            this.modalDescription = "We couldn't find an account with that email. Want to try again?";
             this.openModal();
           }
         },
         error: (error) => {
           console.error('Login Error:', error)
-          this.openModal();
+          
           // alert(error);
           this.modalTitle = "Oops!"
-          this.modalDescription = error;
+          if(error.status == 404) {
+            this.modalDescription = "We couldn't find an account with that email. Want to try again?";
+          }
+          this.openModal();
           this.isLoading = false;
         }
       });
